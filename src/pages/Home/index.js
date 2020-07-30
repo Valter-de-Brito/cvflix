fae52d55234af55b4e94305f6f55546f57dea73d
Valter-de-Brito <valter.dbrito@gmail.com>
2020-07-30 17:24:18 +0100

    Create a new component FormField and modified chech de Slider and Categoria files

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -13,7 +13,9 @@ function Home() {
       <BannerMain
         videoTitle={dadosIniciais.categorias[0].titulo}
         url={dadosIniciais.categorias[0].videos[0].url}
-        videoDescription={"O que é Front-end???"}
+        videoDescription={
+          "O que é Front-end? Trabalhando na área os termos HTML, CSS e JavaScript fazem parte da rotina das desenvolvedoras e desenvolvedores. Mas o que eles fazem, afinal? Descubra com a Vanessa!"
+        }
       />
 
       <Carousel ignoreFirstVideo category={dadosIniciais.categorias[0]} />
@@ -23,6 +25,10 @@ function Home() {
       <Carousel ignoreFirstVideo category={dadosIniciais.categorias[2]} />
 
       <Carousel ignoreFirstVideo category={dadosIniciais.categorias[3]} />
+
+      <Carousel ignoreFirstVideo category={dadosIniciais.categorias[4]} />
+
+      <Carousel ignoreFirstVideo category={dadosIniciais.categorias[5]} />
     </div>
   );
 }
--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,14 +1,76 @@
-import React from "react";
-
-import PageDefault from "../../../components/PageDefault";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
+import PageDefault from "../../../components/PageDefault";
+import FormField from "../../../components/FormField";
+
 function CadastroCategoria() {
+  const valoresIniciais = {
+    nome: "",
+    descricao: "",
+    cor: "",
+  };
+  const [categorias, setCategorias] = useState([]);
+  const [values, setValues] = useState(valoresIniciais);
+
+  function setValue(chave, valor) {
+    // chave: nome, descricao, bla, bli
+    setValues({
+      ...values,
+      [chave]: valor, // nome: 'valor'
+    });
+  }
+
+  function handleChange(event) {
+    setValue(event.target.getAttribute("name"), event.target.value);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    setCategorias([...categorias, values]);
+
+    setValues(valoresIniciais);
+  }
+
   return (
     <PageDefault>
-      <h1>Cadastro de Categoria</h1>
+      <h1>Cadastro de Categoria: {values.nome}</h1>
 
-      <Link to="/">Ir para o Home</Link>
+      <form onSubmit={handleSubmit}>
+        <FormField
+          label="Nome da Categoria"
+          type="text"
+          name="nome"
+          value={values.nome}
+          onChange={handleChange}
+        />
+
+        <FormField
+          label="Descrição:"
+          type="????"
+          name="descricao"
+          value={values.descricao}
+          onChange={handleChange}
+        />
+
+        <FormField
+          label="Cor"
+          type="color"
+          name="cor"
+          value={values.cor}
+          onChange={handleChange}
+        />
+
+        <button>Cadastrar</button>
+      </form>
+
+      <ul>
+        {categorias.map((categoria, indice) => {
+          return <li key={`${categoria}${indice}`}>{categoria.nome}</li>;
+        })}
+      </ul>
+
+      <Link to="/">Ir para home</Link>
     </PageDefault>
   );
 }
