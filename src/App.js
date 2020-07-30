9d372a61e67805165fc7435cb8872b8a87410f62
Valter-de-Brito <valter.dbrito@gmail.com>
2020-07-30 14:44:02 +0100

    Import components to app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import React from "react";
 import Menu from "./components/Menu";
+import BannerMain from "./components/BannerMain";
+import Carousel from "./components/Carousel";
+
+import dadosIniciais from "./data/dados_iniciais.json";
 
 function App() {
   return (
     <div>
       <Menu />
+
+      <BannerMain
+        videoTitle={dadosIniciais.categorias[0].titulo}
+        url={dadosIniciais.categorias[0].videos[0].url}
+        videoDescription={"O que é Front-end???"}
+      />
+
+      <Carousel ignoreFirstVideo category={dadosIniciais.categorias[0]} />
+
+      <Carousel ignoreFirstVideo category={dadosIniciais.categorias[1]} />
+
+      <Carousel ignoreFirstVideo category={dadosIniciais.categorias[2]} />
+
+      <Carousel ignoreFirstVideo category={dadosIniciais.categorias[3]} />
     </div>
   );
 }
